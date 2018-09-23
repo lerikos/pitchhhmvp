c79b268642f2e83bc91672af6902dc1413cd0069
Todd Soulas <todd.soulas.au@gmail.com>
2018-09-23 14:23:20 +1000

    added html

--- app.js
+++ app.js
@@ -2,9 +2,8 @@ const http = require('http');
 const port=process.env.PORT || 3000
 const server = http.createServer((req, res) => {
 res.statusCode = 200;
-res.setHeader('Content-Type', 'text/html');
-res.end('<h1>Hello World</h1>');
-});
+res.render('index')
 server.listen(port,() => {
 console.log(`Server running at port `+port);
 });
+app.set('view engine', 'ejs')
